da69ecb3e972f68b93c5e4c3d378af143862782b
WooRiGyeoRe <rufp0607@naver.com>
2025-01-31 17:19:22 +0900

    메인

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,18 @@
 import 'package:flutter/material.dart';
-import 'package:animate_practice/animate/animate.dart';
+import 'package:animate_practice/animate/animate_screen.dart';
 
 void main() {
   runApp(const MyApp());
 }
 
+// 메인
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: AnimateScreen(),
     );
   }
